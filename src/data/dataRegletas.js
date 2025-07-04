d365e4846e93a2c072d393969b0c35f454ce032f
juanhenaoprogramacion <juanhenaoprogramacion@gmail.com>
2025-07-04 08:32:34 -0500

    updating data

--- src/data/dataRegletas.js
+++ src/data/dataRegletas.js
@@ -7,84 +7,84 @@ export const dataRules =[
     description: '6 tragos de tequila con jugo de naranja licor de durazno y granadina.'
   },
   {
-    id: 1,
+    id: 2,
     name: 'Sex on the Beach',
     value: '$23.000',
     value2: '',
     description: '6 tragos de vodka con jugo de naranja licor de durazno y granadina.'
   },
   {
-    id: 1,
+    id: 3,
     name: 'Bitch Mango',
     value: '$23.000',
     value2: '',
     description: '6 tragos con ginebra, vodka, limón y mango biche.'
   },
   {
-    id: 1,
+    id: 4,
     name: 'Diablo Azul',
     value: '$26.000',
     value2: '',
     description: '6 tragos de vodka con ginebra y curazao azul.'
   },
   {
-    id: 1,
+    id: 5,
     name: 'Cocaína rusa',
     value: '$26.000',
     value2: '',
     description: '6 tragos de vodka con limón, café y azúcar.'
   },
   {
-    id: 1,
+    id: 6,
     name: 'Cocaína Kofla',
     value: '$26.000',
     value2: '',
     description: '6 tragos de vodka, ginebra con limón y sal.'
   },
   {
-    id: 1,
+    id: 7,
     name: 'Lithium',
     value: '$35.000',
     value2: '',
     description: '6 tragos con tequila, whisky, ginebra y amaretto.'
   },
   {
-    id: 1,
+    id: 8,
     name: 'Gin Pepper',
     value: '$30.000',
     value2: '',
     description: '6 tragos de vodka, ginebra con limón, pimienta y azúcar.'
   },
   {
-    id: 1,
+    id: 9,
     name: 'Jaggeranja',
     value: '$65.000',
     value2: '',
     description: ''
   },
   {
-    id: 1,
+    id: 10,
     name: 'Jagger',
     value: '$85.000',
     value2: '',
     description: ''
   },
   {
-    id: 1,
+    id: 11,
     name: 'Jack Daniels',
     value: '$85.000',
     value2: '',
     description: ''
   },
   {
-    id: 1,
+    id: 12,
     name: 'Jagger Bomb',
     value: '$50.000',
     value2: '',
     description: '6 tragos con Jager y energizante.'
   },
   {
-    id: 1,
+    id: 13,
     name: 'Gin Tonic',
     value: '$30.000',
     value2: '',
